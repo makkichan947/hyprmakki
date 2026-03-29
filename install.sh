a2bfdfb03405915517d51227e5c16de36a8189e6
makkichan947 <yakumakki947@hotmail.com>
2026-03-30 01:39:30 +0800

    Update 4

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
 yay -S nwg-look vim-airline vim-airline-themes wlogout glow vscodium snappy-switcher
+
+git clone https://github.com/Mon4sm/Momoisay.git
+cd Momoisay
+sudo sh ./install/linux.sh
+cd ..
+rm -rvf Momoisay
+
+
